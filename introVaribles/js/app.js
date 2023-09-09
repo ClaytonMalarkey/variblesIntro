a37cbfa610be541be6f54d10c93ed9a242ea90c3
ClaytonMalarkey <malarkey1985@gmail.com>
2023-09-08 18:25:05 -0600

    second commit

---
+++ introVaribles/js/app.js
@@ -0,0 +1,5 @@
+var a =10;
+var b =12;
+var stringa ="hello";
+var stringb ="world";
+console.log("printing num 1"+ a + "printing num 2" + b + "printing string a" + stringa + "printing string b" + stringb)
